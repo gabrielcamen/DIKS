6790d309e3f92508d4c203d7d35bd4a4b95d09bc
Ioan-GabrielCamen <g.camen@deltapi.nl>
2022-12-12 13:59:57 +0200

    Added console app to the repo

---
+++ NetFrameWorkConsoleApp/NetFrameWorkConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace NetFrameWorkConsoleApp
+{
+    internal class Program
+    {
+        private static List<int> numbers = null;
+        
+        public static void Main(string[] args)
+        {
+            // numbers ??= new List<int>();
+        }
+    }
+}
